a1da61cb2fb3f8252c848a06b6b544d15e6cb694
binijaaa <sabine.kolesnikova@gmail.com>
2023-04-20 19:16:14 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ExceptionsProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] num = { 1, 2, 3, };
+            //Console.WriteLine(num[10]);
+
+
+            Console.WriteLine("Next will be catch!");
+            try   //we want to implement a mechanism that will handle this exception
+            {
+                int[] numbers = { 1, 2, 3, 4, 5 };
+                Console.WriteLine(numbers[2]);
+                Console.WriteLine(numbers[5]); //this is the exception, out of bounds
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            Console.WriteLine("After catch block");
+
+            Console.WriteLine("+++++++++++++++++++++");
+
+            Console.WriteLine("Next is try catch and final!");
+            try   //we want to implement a mechanism that will handle this exception
+            {
+                int[] numbers = { 1, 2, 3, 4, 5 };
+                Console.WriteLine(numbers[2]);
+                Console.WriteLine(numbers[5]); //this is the exception, out of bounds
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally //whatever happens, this will be executed either way
+            {
+                Console.WriteLine("Work is done!");
+            }
+
+        }
+    }
+}
