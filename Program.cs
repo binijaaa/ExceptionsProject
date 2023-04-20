87d503afcbafe033291031628f8c32cd8befb218
binijaaa <sabine.kolesnikova@gmail.com>
2023-04-20 19:26:10 +0300

    Changes made

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
             //Console.WriteLine(num[10]);
 
 
-            Console.WriteLine("Next will be catch!");
+            Console.WriteLine("Next will be catch!+++++++++++++++++++++");
             try   //we want to implement a mechanism that will handle this exception
             {
                 int[] numbers = { 1, 2, 3, 4, 5 };
@@ -23,20 +23,34 @@
 
             Console.WriteLine("+++++++++++++++++++++");
 
-            Console.WriteLine("Next is try catch and final!");
-            try   //we want to implement a mechanism that will handle this exception
+            Console.WriteLine("Next is try catch and final!+++++++++++++++++++++");
+            try   
             {
                 int[] numbers = { 1, 2, 3, 4, 5 };
                 Console.WriteLine(numbers[2]);
-                Console.WriteLine(numbers[5]); //this is the exception, out of bounds
+                Console.WriteLine(numbers[5]); 
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
             }
-            finally //whatever happens, this will be executed either way
+            finally 
             {
                 Console.WriteLine("Work is done!");
+            }
+
+
+            Console.WriteLine("More than one catch+++++++++++++++++++");
+            try   
+            {
+                int[] numbers = { 1, 2, 3, 4, 5 };
+                Console.WriteLine(numbers[2]);
+                Console.WriteLine(numbers[5]);
+                Console.WriteLine(numbers[22]);
+            }
+            catch (IndexOutOfRangeException indexException)
+            {
+                Console.WriteLine(indexException.ToString());
             }
 
         }
